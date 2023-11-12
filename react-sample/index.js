432e497a39f9afa520473d20e19f41fa60271a74
songzuen <nasjoo71@gmail.com>
2023-11-12 16:42:49 +0900

    body-parser postman

--- react-sample/index.js
+++ react-sample/index.js
@@ -3,11 +3,35 @@ const app = express()                       // function 사용
 const port = 3000                           // port set
 
 //mongodb 연결
+// mongoose.connect('mongodb+srv://songzuen:<password>@cluster0.0ykbmso.mongodb.net/?retryWrites=true&w=majority', {})
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://songzuen:<password>@cluster0.0ykbmso.mongodb.net/?retryWrites=true&w=majority', {})
+mongoose.connect('mongodb+srv://songzuen:Thdwndms95@cluster0.0ykbmso.mongodb.net/?retryWrites=true&w=majority', {})
 .then(() => console.log('MongoDb connected'))
 .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello World 안녕하세요'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const { user } = require('./model/user');
+
+const bodyParser = require('body-parser');
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));             // 클라이언트에서 가져오는 정보를 
+//application/json
+app.use(bodyParser.json)
+
+app.post('/register', (req, res) => {
+    // 회원가입시 필요한 정보 client에서 가져오면
+    // 그것들을 db에 넣어준다
+    
+    //json 형식으로 받음
+    const user = new user(req.body);
+    user.save((err, userInfo) => {                              // save() -> mongoDB 제공 메소드
+        if(err) return res.json({ success : false, err})        // 데이터 전송 실패와 에러메세지 전송
+        return res.status(200).json({                           // 성공코드(200)
+            success : true
+        })
+    });
+})
