f00a60e19d4372b0328aa6c976ea55cbdcec0f47
songzuen <nasjoo71@gmail.com>
2023-11-13 21:02:00 +0900

    postman infinite loading bodyParser의 json()를 json으로 작성

--- react-sample/index.js
+++ react-sample/index.js
@@ -5,7 +5,7 @@ const port = 3000                           // port set
 //mongodb 연결
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://songzuen:<password>@@cluster0.0ykbmso.mongodb.net/?retryWrites=true&w=majority', {})
+mongoose.connect('mongodb+srv://songzuen:<password>@cluster0.0ykbmso.mongodb.net/?retryWrites=true&w=majority', {})
 .then(() => console.log('MongoDb connected'))
 .catch(err => console.log(err));
 
@@ -19,15 +19,16 @@ const bodyParser = require('body-parser');
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended : true}));             // 클라이언트에서 가져오는 정보를 
 //application/json
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 app.post('/register', (req, res) => {
+    console.log(req)
     // 회원가입시 필요한 정보 client에서 가져오면
     // 그것들을 db에 넣어준다
     
     //json 형식으로 받음
-    const user = new user(req.body);
-    user.save((err, userInfo) => {                              // save() -> mongoDB 제공 메소드
+    const user2 = new user(req.body);
+    user2.save((err, userInfo) => {                              // save() -> mongoDB 제공 메소드
         if(err) return res.json({ success : false, err})        // 데이터 전송 실패와 에러메세지 전송
         return res.status(200).json({                           // 성공코드(200)
             success : true
