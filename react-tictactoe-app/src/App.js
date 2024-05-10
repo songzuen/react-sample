4b316926cbbb11b563cc225c41e2911359cd085a
송주은 <nasjoo71@gmail.com>
2024-05-10 09:03:38 +0900

    first commit

---
+++ react-tictactoe-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// 함수형 컴포넌트
+function App() {
+	return (
+		// html이 아닌 jsx를 사용하여 구현
+		// jsx는 js와 html 구조를 같이 사용
+		<div className="game">
+			<div className="game-board">
+			</div>
+			<div className="game-info">
+			</div>
+		</div>
+	);
+}
+export default App;
