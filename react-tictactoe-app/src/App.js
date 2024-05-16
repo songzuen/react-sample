5c6e2636c32276cafa1a2d448156acd5ee58e7e5
songzuen <nasjoo71@gmail.com>
2024-05-16 15:05:09 +0900

    240516 강의 - 동작에 대한 기록 저장하기 - 전개연산자 - map 메서드 - jsxKey 속성 이해 - jumpTo 함수 작성

--- react-tictactoe-app/src/App.js
+++ react-tictactoe-app/src/App.js
@@ -1,19 +1,89 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 
 // 함수형 컴포넌트
 function App() {
+	const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+	const [xIsNext, setXIsNext] = useState(true)
+	const [stepNumber, setStepNumber] = useState(0);
+	// const current = history[history.length - 1];
+	const current = history[stepNumber];
+
+	const calculateWinner = (squares) => {
+		// 모든 경우의 수 나열
+		const lines = [
+			[0,1,2],
+			[3,4,5],
+			[6,7,8],
+			[0,3,6],
+			[1,4,7],
+			[2,5,8],
+			[0,4,8],
+			[2,4,6]
+		]
+
+		for(let index = 0; index < lines.length; index++){
+			const [a,b,c] = lines[index];
+			if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+				return squares[a];
+			}
+		}
+	}
+
+	const winner = calculateWinner(current.squares)
+	let status;
+	if(winner) {
+		status = 'Winner : ' + winner
+	} else {
+		status = `Next player ${xIsNext ? 'X' : 'O'}`
+	}
+
+	const handleClick = (i) => {
+		const newHistory = history.slice(0, stepNumber + 1);
+		const newCurrent = newHistory[newHistory.length - 1];
+		const newSqures = newCurrent.squares.slice();
+
+		if(calculateWinner(newSqures) || newSqures[i]) {
+			return;
+		}
+
+		newSqures[i] = xIsNext ? 'X' : 'O'
+		// 전개연산자 : history에 있는 배열을 늘어뜨림
+		setHistory([...newHistory, { squares: newSqures }]);
+		setXIsNext(prev => !prev)
+
+		setStepNumber(newHistory.length);
+	}
+
+	const jumpTo = (step) => {
+		setStepNumber(step);
+		console.log(stepNumber);
+		setXIsNext(step % 2 === 0)
+	}
+
+	const moves = history.map((step, move) => {
+		const desc = move ? 'Go to move #' + move : 'Go to game start';
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{desc}</button>
+			</li>
+		)
+	})
+
 	return (
 		// html이 아닌 jsx를 사용하여 구현
 		// jsx는 js와 html 구조를 같이 사용
 		<div className="game">
 			<div className="game-board">
-				<Board />
+				<Board squares={current.squares} onClick={(i) => handleClick(i)}/>
 			</div>
 			<div className="game-info">
-				game-info
+				<div><div className='status'>{status}</div> </div>
+				<ol>{moves} </ol>
 			</div>
 		</div>
 	);
 }
+
 export default App;
--- react-tictactoe-app/src/components/Board.js
+++ react-tictactoe-app/src/components/Board.js
@@ -2,65 +2,62 @@ import React, { useState } from 'react'
 import Square from './Squre'
 import './Board.css'
 // 함수형 컴포넌트
-const Board = () => {
+// const Board = () => {
+const Board = ({squares, onClick}) => {
 	// 함수형 컴포넌트
 	// hooks 생성
-	const [squares, setSquares] = useState(Array(9).fill(null))
-	const [xIsNext, setXIsNext] = useState(true)
+	// const [squares, setSquares] = useState(Array(9).fill(null))
+	// const [xIsNext, setXIsNext] = useState(true)
 
-	const handleClick = (i) => {
-		const newSqures = squares.slice();
+	// const handleClick = (i) => {
+	// 	const newSqures = squares.slice();
 
-		if(calculateWinner(newSqures) || newSqures[i]) {
-			return;
-		}
+	// 	if(calculateWinner(newSqures) || newSqures[i]) {
+	// 		return;
+	// 	}
 
-		newSqures[i] = xIsNext ? 'X' : 'O'
-		setSquares(newSqures);
-		setXIsNext(current => !current)
-	}
+	// 	newSqures[i] = xIsNext ? 'X' : 'O'
+	// 	setSquares(newSqures);
+	// 	setXIsNext(current => !current)
+	// }
 
 	const renderSquare = (i) => {
-		return <Square value={squares[i]} onClick={() => handleClick(i)} />
+		// return <Square value={squares[i]} onClick={() => handleClick(i)} />
+		return <Square value={squares[i]} onClick={() => onClick(i)} />
 	}
+	// const calculateWinner = (squares) => {
+	// 	// 모든 경우의 수 나열
+	// 	const lines = [
+	// 		[0,1,2],
+	// 		[3,4,5],
+	// 		[6,7,8],
+	// 		[0,3,6],
+	// 		[1,4,7],
+	// 		[2,5,8],
+	// 		[0,4,8],
+	// 		[2,4,6]
+	// 	]
 
-	const calculateWinner = (squares) => {
-		// 모든 경우의 수 나열
-		const lines = [
-			[0,1,2],
-			[3,4,5],
-			[6,7,8],
-			[0,3,6],
-			[1,4,7],
-			[2,5,8],
-			[0,4,8],
-			[2,4,6]
-		]
+	// 	for(let index = 0; index < lines.length; index++){
+	// 		const [a,b,c] = lines[index];
+	// 		if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+	// 			return squares[a];
+	// 		}
+	// 	}
+	// }
 
-		for(let index = 0; index < lines.length; index++){
-			const [a,b,c] = lines[index];
-			if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-				return squares[a];
-			}
-		}
-	}
-
-	const winner = calculateWinner(squares);
-	let status;
-	if(winner) {
-		status = 'Winner : ' + winner
-	} else {
-		status = `Next player ${xIsNext ? 'X' : 'O'}`
-	}
+	// const winner = calculateWinner(squares);
+	// let status;
+	// if(winner) {
+	// 	status = 'Winner : ' + winner
+	// } else {
+	// 	status = `Next player ${xIsNext ? 'X' : 'O'}`
+	// }
 
 	return (
 		<div>
-			<div className='status'>{status}</div>
+			{/* <div className='status'>{status}</div> */}
 			<div className='board-row'>
-				{/* <button className="square">
-					Square
-				</button> */}
-				{/* <Squre /> */}
 				{renderSquare(0)}
 				{renderSquare(1)}
 				{renderSquare(2)}
