0a34ad261eca0aea4272d2fbe29363fcb512152a
송주은 <nasjoo71@gmail.com>
2024-06-03 10:10:16 +0900

    240531 리액트 강의 context 사용 샘플 어플리케이션

---
+++ react-context-app/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SummaryPage from './pages/SummaryPage';
+import OrderPage from './pages/OrderPage';
+
+function App() {
+  return (
+	<div style={{ padding : '4rem'}}>
+		<OrderPage />
+		<SummaryPage />
+	</div>
+  );
+}
+
+export default App;
---
+++ react-context-app/client/src/components/ErrorBanner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ErrorBanner = ({message}) => {
+	// let message = message || '에러입니다.'
+	return (
+		<div style={{backgroundColor : 'red'}}>message</div>
+	)
+}
+
+export default ErrorBanner
